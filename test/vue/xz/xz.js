596b69c458ed2b21d273f297e2786428aa7205a0
miezuit <miezuit@gmail.com>
2020-04-08 23:29:12 +0300

    tests

--- test/vue/xz/xz.js
+++ test/vue/xz/xz.js
@@ -2,47 +2,41 @@ var app = new Vue({
     el: '#app',
     data: {
         table: [
-            ['', '', ''],
-            ['', '', ''],
-            ['', '', '']
+            '', '', '',
+            '', '', '',
+            '', '', ''
         ],
-        activePlayer: 'X'
+        activePlayer: 'X',
+        winner: false
     },
     methods: {
-        switchPlayer: function() {
+        move(index) {
+            if(this.table[index] != '') return;
+            this.table.splice(index, 1, this.activePlayer);
+            this.switchPlayer();
+            this.checkEndGame();
+        },
+        switchPlayer() {
             this.activePlayer = this.activePlayer == 'X' ? '0' : 'X';
         },
-        move: function(x, y) {
-            this.table[x][y] = this.activePlayer;
-            this.switchPlayer();
-            this.$forceUpdate();
+        checkEndGame() {
+            this.checkComplete((value, index) => index < 3);
+            this.checkComplete((value, index) => index > 2 && index < 6);
+            this.checkComplete((value, index) => index > 5);
+            this.checkComplete((value, index) => index % 3 == 0);
+            this.checkComplete((value, index) => index % 3 == 1);
+            this.checkComplete((value, index) => index % 3 == 2);
+            this.checkComplete((value, index) => index % 3 == 2);
+            this.checkComplete((value, index) => index % 4 == 0);
+            this.checkComplete((value, index) => [2, 4, 6].includes(index));
         },
-
+        checkComplete(filter) {
+            cells = this.table.filter(filter);
+            if(cells.every(cell => cell == 'X') 
+            || cells.every(cell => cell == '0')   
+            ) {
+                this.winner = true;
+            }
+        }
     }
 });
-
-
-function checkEndGame() {
-    let cells = $('.table div').toArray();
-    checkComplete(cells.filter((value, index) => index < 3));
-    checkComplete(cells.filter((value, index) => index > 2 && index < 6));
-    checkComplete(cells.filter((value, index) => index > 5));
-    checkComplete(cells.filter((value, index) => index % 3 == 0));
-    checkComplete(cells.filter((value, index) => index % 3 == 1));
-    checkComplete(cells.filter((value, index) => index % 3 == 2));
-    checkComplete(cells.filter((value, index) => index % 3 == 2));
-    checkComplete(cells.filter((value, index) => index % 4 == 0));
-    checkComplete(cells.filter((value, index) => [2, 4, 6].includes(index)));
-}
-
-/**
- * 
- * @param {Array} cells 
- */
-function checkComplete(cells) {
-    if(cells.every(cell => $(cell).text() == 'X') 
-    || cells.every(cell => $(cell).text() == '0')   
-    ) {
-        $('.end').addClass('visible');
-    }
-}
