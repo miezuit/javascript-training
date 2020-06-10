c6c3f6be1252a80285b16c1f49fabb54b8e74b2b
Radu Gasler <radu.gasler@2checkout.com>
2020-06-11 01:38:15 +0300

    anime

--- test/anim/anim.js
+++ test/anim/anim.js
@@ -8,13 +8,3 @@ anime({
   duration: 2000,
   direction: 'alternate'
 });
-
-anime({
-    targets: 'path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 1500,
-    delay: function(el, i) { return i * 500 },
-    direction: 'alternate',
-    loop: true
-})
