7c6819ed256d1b883272e624b75579975f01ef95
Radu Gasler <radu.gasler@2checkout.com>
2022-01-21 19:25:42 +0200

    testing example with jasmine

---
+++ testing/jasmine-standalone-4.0.0/spec/MathSpec.js
@@ -0,0 +1,16 @@
+describe("Math", function() {
+
+    it("Sum should give the sum of 2 numbers", function() {
+
+        // given:
+        let a = 5
+        let b = 10
+
+        // when:
+        let result = sum(a, b)
+
+        // then:
+        expect(result).toEqual(15)
+      });
+
+})
---
+++ vars/js/vars.js
@@ -0,0 +1,23 @@
+var color = "orange"
+let sticks = 5
+
+function hello() {
+    let city = "Paris"
+    console.log(money, city)
+    var money = 10.5
+
+    {
+       console.log(city);
+       var moo = "Moo"
+       let bzz = "Bzz" 
+    }
+
+    console.log(moo)
+    console.log(bzz)
+}
+
+console.log(color)
+console.log(sticks)
+hello()
+// console.log(city)
+// console.log(money)
